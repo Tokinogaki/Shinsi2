9785c324cc897cc4fd8f3356de795729c3993d4d
Tokinogaki <tokinogaki@gmail.com>
2020-06-14 11:13:36 +0800

    Add exkey login

--- Shinsi2/ViewControllers/LoginVC.swift
+++ Shinsi2/ViewControllers/LoginVC.swift
@@ -4,6 +4,7 @@ import SVProgressHUD
 class LoginVC: UIViewController {
     @IBOutlet var userNameField: UITextField!
     @IBOutlet var passwordField: UITextField!
+    @IBOutlet var exkeyField: UITextField!
     @IBOutlet var loginButton: UIButton!
     @IBOutlet weak var webLoginButton: UIButton!
     
@@ -12,6 +13,7 @@ class LoginVC: UIViewController {
         navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
         userNameField.isHidden = true
         passwordField.isHidden = true
+        exkeyField.isHidden = true
         loginButton.isHidden = true
         webLoginButton.isHidden = true
     }
@@ -24,12 +26,17 @@ class LoginVC: UIViewController {
         } else {
             userNameField.isHidden = false
             passwordField.isHidden = false
+            exkeyField.isHidden = false
             loginButton.isHidden = false
             webLoginButton.isHidden = false
         }
     }
     
     @IBAction func login(sender: AnyObject) {
+        if let exkey = exkeyField.text {
+            exkeyLogin(exkey)
+            return
+        }
         guard let name = userNameField.text, let pw = passwordField.text else { return }
         SVProgressHUD.show()
         RequestManager.shared.login(username: name, password: pw) {
@@ -41,6 +48,17 @@ class LoginVC: UIViewController {
                 SVProgressHUD.showError(withStatus: "Login failed")
                 SVProgressHUD.dismiss(withDelay: 3)
             }
+        }
+    }
+    
+    func exkeyLogin(_ exkey: String) {
+        self.manuallyAddCookie(exKey: exkey)
+        if self.checkCookie() {
+            SVProgressHUD.dismiss()
+            self.pustToList()
+        } else {
+            SVProgressHUD.showError(withStatus: "Login failed")
+            SVProgressHUD.dismiss(withDelay: 3)
         }
     }
     
@@ -75,5 +93,40 @@ class LoginVC: UIViewController {
                 }
             }
         }
+    }
+    
+    func manuallyAddCookie(exKey: String) {
+        let exKeySplitted = exKey.components(separatedBy: "x")
+        guard exKeySplitted.count == 2 else {
+            return
+        }
+        
+        let memberPart = exKeySplitted[0]
+        let memberIdStartIndex = memberPart.index(memberPart.startIndex, offsetBy: 32)
+        let memberIdCookie = createCookie(name: "ipb_member_id", value: String(memberPart[memberIdStartIndex...]))
+        let passHashCookie = createCookie(name: "ipb_pass_hash", value: String(memberPart.prefix(32)))
+        let igneous = createCookie(name: "igneous", value: exKeySplitted[1])
+        
+        let cookieList = [memberIdCookie, passHashCookie, igneous]
+        
+        for theCookie in cookieList {
+            HTTPCookieStorage.shared.setCookie(theCookie)
+            guard var properties = theCookie.properties else {
+                continue
+            }
+            
+            properties[.domain] = ".e-hentai.org"
+            if let newCookie = HTTPCookie(properties: properties) {
+                HTTPCookieStorage.shared.setCookie(newCookie)
+            }
+        }
+    }
+    
+    func createCookie(name: String, value: String) -> HTTPCookie {
+        return HTTPCookie(properties: [.domain: ".exhentai.org",
+        HTTPCookiePropertyKey.name: name,
+        HTTPCookiePropertyKey.value: value,
+        HTTPCookiePropertyKey.path: "/",
+        HTTPCookiePropertyKey.expires: Date(timeInterval: TimeInterval(Int.max), since: Date())])!
     }
 }
